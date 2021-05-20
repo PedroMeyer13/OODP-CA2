e40e6e130fb0dbbc7a7b432b23504b2d34fe8577
PedroMeyer13 <48026831+PedroMeyer13@users.noreply.github.com>
2021-05-20 18:04:12 +0100

    Comment finished

--- src/main/java/Trading/Trading.java
+++ src/main/java/Trading/Trading.java
@@ -10,8 +10,8 @@ public class Trading {
     private Depots buyer = null;
     private Depots seller = null;
     private int buyerControl;
-    private int buyerDepot;
     private int sellerControl;
+    private int buyerDepot;
     private int sellerDepot;
 
     // this method performs the transactions and saves into the depots
@@ -20,7 +20,7 @@ public class Trading {
         // loop that stops when all the available depots cannot trade anymore
         while ((setTrade(companies))) {
                 // check if the buyer has enough money to buy the product
-            if (companies[buyerControl].getDepot(sellerControl).getAllowance() > (companies[buyerDepot].getDepot(sellerDepot).getPrice() + companies[buyerDepot].getDepot(sellerDepot).getDeliveryPrice())) {
+            if (companies[buyerControl].getDepot(buyerDepot).getAllowance() > (companies[sellerControl].getDepot(sellerDepot).getPrice() + companies[sellerControl].getDepot(sellerDepot).getDeliveryPrice())) {
 
                 // add a new transaction to the buyer's record
                 buyer.getTrade().add(newTransaction(buyer.getName(), "Buy", seller.getName(), seller.getNativeProduct().getName(), seller.getPrice() + seller.getDeliveryPrice()));
@@ -28,14 +28,14 @@ public class Trading {
                 seller.getTrade().add(newTransaction(seller.getName(), "Sell", buyer.getName(), seller.getNativeProduct().getName(), seller.getPrice() + seller.getDeliveryPrice()));
 
                 // update the fields of the buyer and seller
-                updateFields(companies[buyerControl], companies[buyerDepot]);
+                updateFields(companies[buyerControl], companies[sellerControl]);
             }
         }
         // return the companies after the trade was done
         return companies;
     }
 
-    // this method checks if any of the depots have space available to buy
+    // this method checks if any of the companies have depots available for the transaction
     private boolean setTrade(Companies[] companies) {
         int size = 20;
         if ((companies[0].getCanBuy().size() < size) && (companies[1].getCanBuy().size() < size)) {
@@ -53,33 +53,42 @@ public class Trading {
         return true;
     }
 
+    // this method will control if a company can trade
     private String isTradable(Companies[] companies) {
+        //
         if (CanTrade(companies)) {
             return "true";
         }
         return "false";
     }
 
+    // this gets two companies and check if they have enough space to buy and sell
     public boolean CanTrade(Companies[] companies) {
 
+        // do while loop to get two different companies
         do {
             buyerControl = getRandomNumber(0, 2);
-            buyerDepot = getRandomNumber(0, 2);
-        } while (buyerControl == buyerDepot);
+            sellerControl = getRandomNumber(0, 2);
+            // keep going while the companies are the same
+        } while (buyerControl == sellerControl);
 
-        sellerControl = getRandomNumber(1, companies[buyerControl].getCanBuy().size());
-        buyer = companies[buyerControl].getDepot(sellerControl);
+        // get a random depot
+        buyerDepot = getRandomNumber(1, companies[buyerControl].getCanBuy().size());
+        buyer = companies[buyerControl].getDepot(buyerDepot);
+
+        // gets a random seller
         sellerDepot = getRandomNumber(1, companies[buyerControl].getCanBuy().size());
-        seller = companies[buyerDepot].getDepot(sellerDepot);
+        seller = companies[sellerControl].getDepot(sellerDepot);
 
+        // checks if they have enough space to buy and sell the products
         if ((buyer.getExternalProduct() > 39) || (seller.getNativeStock() < 14)) {
             return false;
         }
         return true;
     }
 
+    // method that creates a new transaction object that will be stored in the depots
     public TradeInfo newTransaction(String company, String transactionType, String client, String product, int total) {
-
         TradeInfo trade = new TradeInfo.TradeInfoBuilder(
                 company,
                 transactionType,
@@ -88,19 +97,28 @@ public class Trading {
         return trade;
     }
 
+    // this method generates and returns a random number
     private int getRandomNumber(int min, int max) {
         int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
         return random_int;
     }
 
+    // this method updates the information of the depots after a transaction
     private void updateFields(Companies companyBuyer, Companies companySeller) {
-//        System.out.println(buyer.getExternalProduct());
+
+        // add a new external product to the buyer
         buyer.setExternalProduct(buyer.getExternalProduct() + 1);
+
+        // if the buyer has more than 39 external products it removes them from the available depots to make transactions
         if (buyer.getExternalProduct() > 39) {
-            companyBuyer.removeItemCanBuy(sellerControl);
+            companyBuyer.removeItemCanBuy(buyerDepot);
         }
+        // set the seller stock to match the sold stock
         seller.setNativeStock(seller.getNativeStock() - 1);
+        // add money from the cash balance after the transaction
         seller.setCashBalance(seller.getPrice() + seller.getDeliveryPrice());
+
+        // if the depot cant sell anymore, removes them from the available depots for transaction
         if (buyer.getNativeStock() < 16) {
             companySeller.removeItemCanBuy(sellerDepot);
         }
